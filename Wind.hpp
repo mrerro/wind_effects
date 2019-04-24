715894177ee5aa46d8566cb871612eedabec4728
mrerro <dyakov.hellvisionstudio@gmail.com>
2019-04-24 22:33:03 +0500

    fix ;

--- Wind.hpp
+++ Wind.hpp
@@ -28,13 +28,13 @@ public:
     virtual std::vector<double> F(double t, std::vector<double> &Y);
 
     double Step(int);
-    double get_x() { return Y[0]; };
-    double get_y() { return Y[1]; };
-    double get_z() { return Y[2]; };
+    double get_x() { return Y[0]; }
+    double get_y() { return Y[1]; }
+    double get_z() { return Y[2]; }
     double get_path() { return std::sqrt(std::pow(Y[0], 2) + std::pow(Y[1],2)); }
-    double get_Vx() { return Y[3]; };
-    double get_Vy() { return Y[4]; };
-    double get_Vz() { return Y[5]; };
+    double get_Vx() { return Y[3]; }
+    double get_Vy() { return Y[4]; }
+    double get_Vz() { return Y[5]; }
 };
 
 #endif
